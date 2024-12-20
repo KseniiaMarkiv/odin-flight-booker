57b3fdf16165119db142cc10532ab6a0ae60412a
KseniiaMarkiv <ksenya.markiv@gmail.com>
2024-12-20 18:12:04 -0500

    Fixed Time Zone. Implemented selection feature for dropdown menus of Search form

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,9 +1,26 @@
 class FlightsController < ApplicationController
   def index
-    @available_dates = Flight.select(:start_time).distinct.pluck(:start_time).map(&:to_date)
-    @selected_date = params[:date] ? params[:date].to_date : nil
-    @flights = @selected_date ? Flight.available_on(@selected_date) : Flight.all
+    # @available_dates = Flight.select(:start_time).pluck(:start_time).map(&:to_datetime)
+    # @selected_date = params[:date].present? ? params[:date].to_datetime : nil
+    # @flights = @selected_date ? Flight.available_on(@selected_date) : Flight.all
 
+    # Retrieve all airports for the dropdowns
     @airports = Airport.all
+
+    # Filter flights based on selected parameters
+    @departure_airport_id = params[:departure_airport_id]
+    @arrival_airport_id = params[:arrival_airport_id]
+    @selected_date = params[:date].present? ? params[:date].to_datetime : nil
+
+    @flights = Flight.all
+
+    # Apply filters if parameters are present
+    @flights = @flights.where(departure_airport_id: @departure_airport_id) if @departure_airport_id.present?
+    @flights = @flights.where(arrival_airport_id: @arrival_airport_id) if @arrival_airport_id.present?
+    @flights = @flights.available_on(@selected_date) if @selected_date.present?
+
+    # For the date picker min/max values
+    @available_dates = Flight.select(:start_time).distinct.pluck(:start_time).map(&:to_datetime)
   end
 end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,34 @@
+# Check if the database adapter is PostgreSQL
+if ActiveRecord::Base.connection.adapter_name.downcase == 'postgresql'
+  # Clear all users and reset the primary key sequence
+  Flight.destroy_all
+  Airport.destroy_all
+  ActiveRecord::Base.connection.reset_pk_sequence!('flights')  # For PostgreSQL
+  ActiveRecord::Base.connection.reset_pk_sequence!('airports')  # For PostgreSQL
+
+  puts "PostgreSQL database detected: Cleared all flights, airports and reset primary key sequence."
+else
+  puts "Non-PostgreSQL database detected: Skipping flights, airports clearance and primary key reset."
+end
+
 airports = %w[SFO NYC LAX ORD ATL MIA]
 airports.each { |code| Airport.create!(code: code) }
 
+# Create flights with random start times
 50.times do
+  departure_airport = Airport.all.sample
+  arrival_airport = (Airport.all - [departure_airport]).sample
+  start_time = Faker::Time.forward(days: 30, period: :morning).in_time_zone('Eastern Time (US & Canada)')
+
   Flight.create!(
-    departure_airport: Airport.all.sample,
-    arrival_airport: Airport.all.sample,
-    start_time: Faker::Time.forward(days: 30, period: :morning),
-    duration: rand(60..300)
+    departure_airport: departure_airport,
+    arrival_airport: arrival_airport,
+    start_time: start_time,
+    duration: rand(1..5)
   )
+
+  puts "Created flight: #{departure_airport.code} -> #{arrival_airport.code}, Start Time: #{start_time}, Duration: #{rand(1..5)} hours"
 end
+
+puts "Seeding completed!"
+puts "Date & Time now #{DateTime.now()}"
