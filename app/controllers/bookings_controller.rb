06eadf451a46d23dd2f0cc9d358568997636c86f
KseniiaMarkiv <ksenya.markiv@gmail.com>
2024-12-27 17:36:49 -0500

    Added callback to prevent redirecting to Booking page without full information

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,12 +1,20 @@
 class BookingsController < ApplicationController
+  before_action :validate_search_params, only: [:new]
+
   def new
     @flight = Flight.find(params[:flight_id])
     @passengers = params[:passengers].to_i
   end
 
   def create
-    # Handle booking creation logic here
-    # Example: redirect to a confirmation page
     redirect_to root_path, notice: "Booking successfully created!"
+  end
+
+  private 
+
+  def validate_search_params
+    if params[:flight_id].blank? || params[:passengers].blank?
+      redirect_to root_path, alert: "Please select a flight and specify the number of passengers before proceeding."
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   root "flights#index"
-  # get 'flights/reset', to: 'flights#reset', as: 'reset_flights'
-  # resources :flights, only: [:index]
-
   resources :flights, only: [:index] do
     collection do
       get :reset
