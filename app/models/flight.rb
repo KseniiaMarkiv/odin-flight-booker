b30989489ff74516b0752a9f3ace722f1d0aa0d8
KseniiaMarkiv <ksenya.markiv@gmail.com>
2025-01-06 13:17:12 -0500

    Set up associations between Bookings, Passengers, and Flights

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,6 +1,7 @@
 class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: 'Airport'
   belongs_to :arrival_airport, class_name: 'Airport'
-
+  has_many :bookings, dependent: :destroy
+  
   scope :available_on, ->(date) { where(start_time: date.all_day) }
 end
