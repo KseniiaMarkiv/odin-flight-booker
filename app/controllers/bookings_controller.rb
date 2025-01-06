e13215c6be03efdc859c236d7cdcb8aeac59f374
KseniiaMarkiv <ksenya.markiv@gmail.com>
2025-01-06 14:10:03 -0500

    Rewrote Booking new action and added nested attributes

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,10 +4,13 @@ class BookingsController < ApplicationController
   def new
     @flight = Flight.find(params[:flight_id])
     @passengers = params[:passengers].to_i
+    @booking = Booking.new
+    @passengers.times { @booking.passengers.build }
   end
 
   def create
-    redirect_to root_path, notice: "Booking successfully created!"
+    @booking = Booking.new(booking_params)
+    redirect_to root_path, notice: "Booking successfully created!" if @booking.save
   end
 
   private 
@@ -16,5 +19,9 @@ class BookingsController < ApplicationController
     if params[:flight_id].blank? || params[:passengers].blank?
       redirect_to root_path, alert: "Please select a flight and specify the number of passengers before proceeding."
     end
+  end
+
+  def booking_params
+    params.require(:booking).permit(:flight_id, passengers_attributes: [:name, :email])
   end
 end
